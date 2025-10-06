bd448003aac9e2e38d67cfcb280eebb44f1331f0
Hristo Mirkov <mirkovh2@gmail.com>
2025-10-06 02:39:46 +0000

    2025-10-05

---
+++ models/staging/STG_ABC_BANK_POSITION.sql
@@ -0,0 +1,27 @@
+
+WITH src_data as (
+SELECT 
+   ACCOUNTID   as ACCOUNT_CODE, 
+   SYMBOL as SECURITY_CODE, 
+   DESCRIPTION as SECURITY_NAME, 
+   EXCHANGE as EXCHANGE_CODE, 
+   REPORT_DATE as REPORT_DATE, 
+   QUANTITY as QUANTITY, 
+   COST_BASE as COST_BASE, 
+   POSITION_VALUE as POSITION_VALUE, 
+   CURRENCY as CURRENCY_CODE, 
+   'SOURCE_DATA.ABC_BANK_POSITION' as RECORD_SOURCE  
+ FROM {{ source ( 'abc_bank', 'ABC_BANK_POSITION')}}   
+), 
+hashed as (
+   SELECT 
+      concat_ws ('|', ACCOUNT_CODE, SECURITY_CODE) as POSITION_KEY, 
+      concat_ws ('|', ACCOUNT_CODE, SECURITY_CODE, 
+           SECURITY_NAME, EXCHANGE_CODE, REPORT_DATE, 
+           QUANTITY, COST_BASE, POSITION_VALUE, CURRENCY_CODE)
+              as POSITION_HDIFF, 
+      *, 
+      '{{ run_started_at }}' as LOAD_TS_UTC   
+    FROM src_data       
+)
+SELECT * from hashed
